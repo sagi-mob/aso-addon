b7a038d772acf44b513ad170f1800bbcff4859c8
Sagi Levi <lesagi@gmail.com>
2019-10-06 17:41:40 +0300

    Permutation count works, but need to seperate filtering

--- src/rec-table-gen.js
+++ src/rec-table-gen.js
@@ -1,19 +1,16 @@
-import { flatten, reduce, findIndex, filter } from 'ramda';
-// import { getAppFieldCtor } from './services/PropService';
+import { flatten, reduce, findIndex, filter, zipObj, map } from 'ramda';
+import { getAppFieldCtor } from './services/PropService';
 import { DailyKeywordsRanking } from './services/ApiService';
+import { escapeRegExp } from './utils';
 
+const duplicateRemoveFilter = (curr, ind, array) => array.indexOf(curr) === ind;
 const refactorArr = cellsTextArr =>
   reduce(
     (acc, curr) => {
       const currLower = curr.toLowerCase().trim();
-      if (curr.indexOf(',') !== -1) {
-        return acc.concat(currLower.split(new RegExp(/\W+/gm)));
-      }
-      if (curr.indexOf(' ') !== -1) {
-        // return acc.concat([currLower, ...currLower.split(new RegExp(/\W+/gm))]);
-        return acc.concat(currLower.split(new RegExp(/\W+/gm)));
-      }
-      return acc.concat(currLower);
+      const regex = new RegExp(/\W+/gm);
+      const toAdd = currLower.search(regex) ? currLower.split(regex) : currLower;
+      return acc.concat(toAdd);
     },
     [],
     cellsTextArr
@@ -37,29 +34,41 @@ const getWordsList = (ss, sheet, textRanges) => {
       .filter(curr => curr[2] === 3)
       .map(curr => curr[0]);
     const combined = cellsTextArr.concat(relevanceTable);
-    return combined.filter((curr, ind) => combined.indexOf(curr) === ind).sort();
+    return combined.filter(duplicateRemoveFilter).sort();
   } catch (e) {
     return Error(`Tab: Category, Relevance & Chosen
     does not exist. The algorithm can't run without it`);
   }
 };
 
-// const getApiKeywordsList = (ss, mmpid, cCode) => {
-//   const getField = getAppFieldCtor(mmpid);
-//   const name = getField(`nickname`) || getField(`name`);
-//   const os = getField('os');
+const getApiSheetWordsList = (ss, mmpid, cCode) => {
+  const getField = getAppFieldCtor(mmpid);
+  const name = getField(`nickname`) || getField(`name`);
+  const os = getField('os');
 
-//   const apiSheet = ss.getSheetByName(`API ${name} ${os} ${cCode.toUpperCase()}`);
-//   const keywordsApiTable = apiSheet.getDataRange().getValues();
-//   const headers = keywordsApiTable[0].map(curr =>
-//     curr instanceof Date ? 'rank' : curr.toString()
-//   );
-//   const keywordsApiObj = keywordsApiTable.slice(1).map(curr => zipObj(headers, curr));
-//   return keywordsApiObj;
-// };
+  const apiSheet = ss.getSheetByName(`API ${name} ${os} ${cCode.toUpperCase()}`);
+  const keywordsApiTable = apiSheet.getDataRange().getValues();
+  const headers = keywordsApiTable[0].map(curr =>
+    curr instanceof Date ? 'rank' : curr.toString()
+  );
+  const keywordsApiObj = keywordsApiTable.slice(1).map(curr => zipObj(headers, curr));
+  return keywordsApiObj;
+};
 
-const findAndRemoveCtor = list => word => {
-  const finder = findIndex(el => el.keyword.search(new RegExp(`\\b${word}\\b`, `gim`)) !== -1);
+/*  
+  A functor - first, get a list as an argument.
+  Then, for each word given as an argument, it takes all the keywords in
+  the list that have this wors as a substring oh the phrase. and removes them
+  from the original list
+  Purpose - once a phrase met a single criteria its already taken in count
+  no need to take it into consideration when checking other phrases and therefore
+  it should be removed
+*/
+const findAndExtractCtor = list => word => {
+  const escapedWord = escapeRegExp(word);
+  const finder = findIndex(
+    el => el.keyword.search(new RegExp(`\\b${escapedWord}\\b`, `gim`)) !== -1
+  );
   let ind = finder(list);
   let res = [];
   while (ind !== -1) {
@@ -69,7 +78,7 @@ const findAndRemoveCtor = list => word => {
   return res;
 };
 
-const operatorObj = () => {
+const applyPrimOpCtor = () => {
   const obj = {
     gt: (val1, val2) => val1 > val2,
     gte: (val1, val2) => val1 >= val2,
@@ -81,38 +90,104 @@ const operatorObj = () => {
   return key => obj[key];
 };
 
-const fieldObj = () => {
-  const obj = {
-    traffic: 'searchVolume',
-    difficulty: 'chance',
-    competition: 'numberOfApps',
-    rank: 'rank'
+/*
+  all fields that are given by mobile action or can be calculated by 
+  applying ptimitive operators are considered to be 'primitive'
+*/
+const getPrimFieldCtor = keyword => {
+  const fieldObj = {
+    traffic: keyword.searchVolume,
+    difficulty: keyword.chance,
+    competition: keyword.numberOfApps,
+    rank: keyword.rank,
+    tdl: keyword.searchVolume / keyword.keyword.length // tdl = Traffic divided by Length
   };
-  return key => obj[key];
+  return key => fieldObj[key];
 };
 
-const filterListCtor = conds => {
-  const applyOp = operatorObj();
-  const getField = fieldObj();
-  return key =>
-    reduce(
-      (acc, cond) => acc && applyOp(cond.cond)(key[getField(cond.field)], cond.val),
-      true,
+const getSpecialFieldCtor = (oldKeywordsList, newKeywordsList) => {
+  const keywordsString = map(keyword => keyword.keyword, newKeywordsList).join(',');
+  const newKeywordsObj = reduce(
+    (acc, keyword) => {
+      acc[keyword.keyword] = keyword;
+      return acc;
+    },
+    {},
+    newKeywordsList
+  );
+  const oldKeywordsObj = reduce(
+    (acc, keyword) => {
+      acc[keyword.keyword] = keyword;
+      return acc;
+    },
+    {},
+    oldKeywordsList
+  );
+  return {
+    pc: keyword => {
+      const escapedWord = escapeRegExp(keyword);
+      const matches = keywordsString.match(new RegExp(`\\b${escapedWord}\\b`, `gim`));
+      return matches ? matches.length : 0;
+    },
+    rd: keyword => newKeywordsObj[keyword].rank - oldKeywordsObj[keyword].rank
+  };
+};
+
+const filterListCtor = (oldKeywordsList, newKeywordsList, conds) => {
+  const applyOp = applyPrimOpCtor();
+  const getSpecialField = getSpecialFieldCtor(oldKeywordsList, newKeywordsList);
+  return keyword => {
+    const getPrimField = getPrimFieldCtor(keyword);
+    return reduce(
+      (acc, cond) =>
+        acc ||
+        applyOp(cond.cond)(
+          getPrimField(cond.field) || getSpecialField[cond.field](keyword.keyword),
+          cond.val
+        ),
+      false,
       conds
     );
+  };
 };
 
-const createRecommendations = (mmpid, cCode, textRanges, conds) => {
+// const primCondFilter = cond =>
+//   cond.field === 'traffic' ||
+//   cond.field === 'difficulty' ||
+//   cond.field === 'competition' ||
+//   cond.field === 'rank' ||
+//   cond.field === 'tdl';
+
+// const specialCondFilter = cond => cond.field === 'pc' || cond.field === 'rd';
+
+const createRecommendations = (mmpid, cCode, textRanges, comparisonDate, conds) => {
   const ss = SpreadsheetApp.getActive();
   const sheet = ss.getActiveSheet();
 
+  // const primConds = conds.filter(primCondFilter);
+  // const specialConds = conds.filter(specialCondFilter);
+
+  // Extract words in text assets and also Relevant keywords from 'relevance' tab
   const wordsList = getWordsList(ss, sheet, textRanges);
-  const apiKeywordsList = DailyKeywordsRanking(mmpid, cCode);
-  const filtering = filterListCtor(conds);
-  const filteredKeys = filter(filtering, apiKeywordsList);
-  const remover = findAndRemoveCtor(filteredKeys);
-  const unfiltered = reduce((acc, curr) => acc.concat(remover(curr)), [], wordsList);
-  return unfiltered.map(key => key.keyword).join('\n');
+  // get all keywords of given app
+  const oldKeywordsList = DailyKeywordsRanking(mmpid, cCode, comparisonDate);
+  const newKeywordsList = getApiSheetWordsList(ss, mmpid, cCode);
+  if (oldKeywordsList.length !== newKeywordsList.length) {
+    throw Error('The API sheet is not updated with the latest keywords');
+  }
+
+  // Fetches all keywords that fulfill one of the given primitive conditions
+  const filtering = filterListCtor(oldKeywordsList, newKeywordsList, conds);
+  const primitiveFiltered = filter(filtering, newKeywordsList);
+  //
+  const extractor = findAndExtractCtor(primitiveFiltered);
+  const unfiltered = reduce((acc, curr) => acc.concat(extractor(curr)), [], wordsList);
+  return unfiltered
+    .map(key => key.keyword)
+    .concat(wordsList)
+    .filter(duplicateRemoveFilter)
+    .sort()
+    .join('\n');
 };
 
 export default createRecommendations;
--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,27 @@
 const include = filename => HtmlService.createHtmlOutputFromFile(filename).getContent();
-const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
-const numToChar = (num) => 'abcdefghijklmnopqrstuvwxyz'.charAt(num - 1);
+const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
+const numToChar = num => 'abcdefghijklmnopqrstuvwxyz'.charAt(num - 1);
 
 function binarySearch(a, key, comparator) {
-  let output = -1; // default (not found) value
+  const output = -1; // default (not found) value
   let low = 0;
   let high = a.length - 1;
-  while ((low <= high)) {
+  while (low <= high) {
     const middle = Math.floor((low + high) / 2);
     const val = a[middle];
-    if ((comparator != 'undefined' && comparator(key, val) == 0) || val == key) {
+    if ((comparator !== 'undefined' && comparator(key, val) === 0) || val === key) {
       return middle;
-    } else if ((comparator != 'undefined' && comparator(key, val) < 0) || key < val)
-      high = middle - 1;
-    else if ((comparator != 'undefined' && comparator(key, val) > 0) || true) low = middle + 1;
+    }
+    if ((comparator !== 'undefined' && comparator(key, val) < 0) || key < val) high = middle - 1;
+    else if (comparator !== 'undefined' && comparator(key, val) > 0) {
+      low = middle + 1;
+    }
   }
   return output;
 }
 
-export { include, capitalize, numToChar, binarySearch }
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
 
+export { include, capitalize, numToChar, binarySearch, escapeRegExp };
