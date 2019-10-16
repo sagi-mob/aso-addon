a15826256f6d2bfb8e05709ff14d643f985d9be5
Sagi Levi <lesagi@gmail.com>
2019-10-16 13:05:34 +0300

    COUNT IF FORMULAS

--- src/Formulas/formulas.js
+++ src/Formulas/formulas.js
@@ -125,3 +125,49 @@ function encapsulateCriterion(criterion) {
   };
 }
 
+/**
+ * For each cell in {searchCol} that contains the cell content, it adds
+ * the correspond value from the same row in {sumCol}
+ * {criterion} is optional if want to add a condition the the {sumCol} value also need to fullfill.
+ * @param {keywordsRange} input The column to check and iterate over.
+ * @param {searchRange} input The column to check and iterate over.
+ * @return The sum of all cells that met the requirements.
+ * @customfunction
+ */
+function COUNTIFRANGE(keywordsRange, searchRange, criterionRange, criterion) {
+  var checkCriteria =
+    criterion != null
+      ? encapsulateCriterion(criterion)
+      : function(val) {
+          return true;
+        };
+  var arrayOfCounts = keywordsRange.map(function(cell) {
+    var counter = 0;
+    for (var i = 0; i < searchRange.length; i++) {
+      var criterionRangeVal = criterionRange ? criterionRange[i] : 0 ;
+      var phrase = searchRange[i].toString();
+      var escapedCell = escapeRegExp(cell.toString()); // function can be found in utils.js
+      var found = phrase.search(new RegExp('\\b('+escapedCell+')\\b', 'gim')) != -1;
+      counter += checkCriteria(criterionRangeVal) && found ? 1 : 0;
+    }
+    return counter;
+  });
+  return arrayOfCounts;
+}
+
+/**
+ * @param {cell} input The column to check and iterate over.
+ * @param {searchRange} input The column to check and iterate over.
+ * @return The number of all cells the {cell} is a substring of.
+ * @customfunction
+ */
+function COUNTIFCELL(cell, searchRange) {
+    var counter = 0;
+    var escapedCell = escapeRegExp(cell.toString()); // function can be found in utils.js
+    for (var i = 0; i < searchRange.length; i++) {
+      var phrase = searchRange[i].toString();
+      var found = phrase.search(new RegExp('\\b'+escapedCell+'\\b', 'gim')) != -1;
+      counter += found ? 1 : 0;
+    }
+    return counter;
+}
